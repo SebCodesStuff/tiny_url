d3399074ff392a3fa1bb432584925375c59cc6b0
vagrant <seb.namek@gmail.com>
2017-10-10 21:00:44 +0000

    second commit: Server accesses database of urls and displays their short form in a list, or by itself with the codes url

--- express_server.js
+++ express_server.js
@@ -12,6 +12,22 @@ var urlDatabase = {
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
+console.log(urlDatabase);
+
+//This is used to add the urlDatabase to url_index
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render('./pages/urls_index', templateVars)
+})
+
+//This is used to write to urls_show, when accessing longURL it must be done in [] notation
+//This is because if you do dot notation it's like you are navigating through the object
+//and urlDatabase does not contain a property called req. [] notation however accesses a property
+//matching the value you enter
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("./pages/urls_show", templateVars);
+});
 
 //.json is property of the express function check docs for more info
 app.get("/urls.json", (req, res) => {
@@ -25,3 +41,5 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+//<% for (myURL in shortURL) {%> <%=myURL %> <% } %>
