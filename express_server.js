d7bfddc4d7245fd4b6722c3853c8c0ea1b2415a7
vagrant <seb.namek@gmail.com>
2017-10-11 23:16:09 +0000

    Completed the cookies branch

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set('view engine',"ejs");
+app.use(cookieParser())
 
 // The order of our routing is important. Express goes through these in order and stops at the first case
 //that matches
@@ -23,7 +24,7 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let urls = tinyDB.getAll();
   console.log(req.cookies)
-  res.render('./pages/urls_index', {urls: urls})
+  res.render('./pages/urls_index', {urls: urls, username: req.cookies.username})
 });
 
 
@@ -33,32 +34,31 @@ app.post("/urls", (req, res) => {
   let shortURL = tinyDB.generateRandomString();
   let longURL = req.body.longURL
   tinyDB.add(tinyDB.urlDatabase, shortURL, longURL);
-  let urls = tinyDB.getAll();
-
-  res.render('./pages/urls_index', {urls: urls});
+  // let urls = tinyDB.getAll();
+  res.render('./pages/urls_show', {shortURL, longURL, username: req.cookies.username})
 });
 
 
 app.post("/urls/:id/delete", (req, res) => {
   delete tinyDB.urlDatabase[req.params.id];
   let urls = tinyDB.getAll();
-  res.render('./pages/urls_index', {urls: urls});
+res.render('./pages/urls_index', {urls: urls, username: req.cookies.username})
 });
 
 app.post("/urls/:id/edit", (req, res) => {
   // console.log(res.body.shortURL);
   tinyDB.update(req.params.id, req.body.shortURL)
   let urls = tinyDB.getAll();
-  res.render('./pages/urls_index', {urls: urls});
+  res.render('./pages/urls_index', {urls: urls, username: req.cookies.username})
 });
 
 
 //This is my login form response, creates a cookie and adds the
 //username to an object
 app.post("/login", (req, res) => {
-  res.cookie("Username" ,req.body.username, {expires: new Date(Date.now() + 900000)})
+  res.cookie("username" ,req.body.username, {expires: new Date(Date.now() + 900000)})
   let urls = tinyDB.getAll();
-  res.render('./pages/urls_index', {urls: urls});
+  res.render('./pages/urls_index', {username: req.cookies.username, urls: urls})
 });
 
 //This is my redirecter
@@ -69,7 +69,7 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("./pages/urls_new");
+  res.render("./pages/urls_new", {username: req.cookies.username}) ;
 });
 //
 // //This is used to write to urls_show, when accessing longURL it must be done in [] notation
@@ -79,7 +79,7 @@ app.get("/urls/new", (req, res) => {
 //
 //
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, longURL: tinyDB.urlDatabase[req.params.id]};
+  let templateVars = { shortURL: req.params.id, longURL: tinyDB.urlDatabase[req.params.id], username: req.cookies.username};
   res.render("./pages/urls_show", templateVars);
 });
 
